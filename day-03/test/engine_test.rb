5f258bef60c62de932187823a5438b855c68d22b
Toby Chin <tchin@brotherhoodmutual.com>
2023-12-19 09:25:57 -0500

    Skip day 3 tests since I am not attempting that challenge right now

--- day-03/test/engine_test.rb
+++ day-03/test/engine_test.rb
@@ -7,22 +7,26 @@ class TestEngine < Minitest::Test
   end
 
   def test_part_numbers_are_found
+    skip "moved on to other challenges"
     engine = Engine.new(@schematic)
     assert_equal [467, 35, 633, 617, 592, 755, 664, 598], engine.part_numbers
   end
 
   def test_part_number_sum
+    skip "moved on to other challenges"
     engine = Engine.new(@schematic)
     assert_equal 4631, engine.part_number_sum
   end
 
   def test_get_number_finds_a_full_number
+    skip "moved on to other challenges"
     str = "419..467.."
     engine = Engine.new(@schematic)
     assert_equal 419, engine.get_number(str, 0)
   end
 
   def test_get_number_finds_a_full_number_in_the_middle_of_a_string
+    skip "moved on to other challenges"
     str = "419..467.."
     engine = Engine.new(@schematic)
     assert_equal 467, engine.get_number(str, 6)
